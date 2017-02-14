93e4eefa26b9f14779947326af3ad35006d8f989
kj179602 <kaj915@wp.pl>
2017-02-14 19:05:27 +0100

    test_3_ref

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -3,6 +3,7 @@ package edu.iis.mto.serverloadbalancer;
 
 public class Server {
 
+	private static final double MAXIMUM_LOAD = 100.0d;
 	public double currentLoadPecentage;
 	public int capacity;
 	
@@ -13,6 +14,11 @@ public class Server {
 
 	public boolean contains(Vm vm) {
 		return true;
+	}
+
+	public void addVm(Vm vm) {
+		currentLoadPecentage = (double) vm.size / (double) capacity * MAXIMUM_LOAD;
+		
 	}
 
 }
